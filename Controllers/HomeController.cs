0e04c9a261773cb55b1d6b923c629852db7b2569
a1138143@uabc.edu.mx <a1138143@uabc.edu.mx>
2021-10-26 19:51:07 -0700

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practica_MVC_4.Models;
+
+namespace Practica_MVC_4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FormularioVisita()
+        {
+            return View();
+        }
+
+        public ActionResult CargarDatos()
+        {
+            string nombre = Request.Form["nombre"].ToString();
+            String comentarios = Request.Form["cometarios"].ToString();
+            LibroVisitas libro = new LibroVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        
+        }
+
+
+
+
+    }
+
+}
